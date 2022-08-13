3d7ba29349fdea9951b0bb2538e06cf78f74955a
Léo Belzile <belzilel@gmail.com>
2022-08-12 21:49:53 -0400

    Version initiale

---
+++ _targets.R
@@ -0,0 +1,28 @@
+library(targets)
+library(tarchetypes)
+suppressPackageStartupMessages(library(dplyr))
+
+options(tidyverse.quiet = TRUE,
+        dplyr.summarise.inform = FALSE)
+
+tar_option_set(
+  packages = c("tibble"),
+  format = "rds",
+  workspace_on_error = TRUE
+)
+
+# here::here() returns an absolute path, which then gets stored in tar_meta and
+# becomes computer-specific (i.e. /Users/andrew/Research/blah/thing.Rmd).
+# There's no way to get a relative path directly out of here::here(), but
+# fs::path_rel() works fine with it (see
+# https://github.com/r-lib/here/issues/36#issuecomment-530894167)
+here_rel <- function(...) {fs::path_rel(here::here(...))}
+
+ list(
+  ## Class schedule file ----
+  tar_target(schedule_file, here_rel("files", "horaire.csv"), format = "file"),
+
+
+  ## Build site ----
+  tar_quarto(site, path = ".")
+)
