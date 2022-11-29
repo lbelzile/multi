0fab21c509c0eb84ac7ece45552b1dfbc338b8f7
Léo Belzile <belzilel@gmail.com>
2022-11-29 00:38:36 -0500

    Diapos via targets

--- _targets.R
+++ _targets.R
@@ -29,31 +29,34 @@ source("R/tar_slides.R")
   # Use dynamic branching to get a list of all .Rmd files in slides/ and knit them
   #
   # The main index.qmd page loads xaringan_slides as a target to link it as a dependency
-  ### Convert xaringan HTML slides to PDF ----
-  #
   # Use dynamic branching to get a list of all knitted slide .html files and
   # convert them to PDF with pagedown
   #
   # The main index.qmd page loads xaringan_pdfs as a target to link it as a dependency
-  #tar_files(xaringan_html_files, {
-  #  xaringan_slides
-  #  list.files(here_rel("diapos"),
-  #             pattern = "\\.html",
-  #             full.names = TRUE)
-  #}),
-  #tar_target(xaringan_pdfs,
-  #           xaringan_to_pdf(xaringan_html_files),
-  #           pattern = map(xaringan_html_files),
-  #           format = "file"),
+  tar_files(xaringan_files, list.files(here_rel("diapos"),
+                                       pattern = "\\.Rmd",
+                                       full.names = TRUE)),
+  tar_target(xaringan_slides,
+             render_xaringan(xaringan_files),
+             pattern = map(xaringan_files),
+             format = "file"),           
+             
+    tar_files(quarto_files, list.files(here_rel("diapos"),
+                                       pattern = "\\.qmd",
+                                       full.names = TRUE)),
+  tar_target(slide_pdfs,
+             quarto::quarto_render(quarto_files),
+             pattern = map(quarto_files),
+             format = "file"),
 
-  tar_files(path_eval_quarto,
-    list.files(here_rel("evaluations"),
-               pattern = "\\.qmd",
-               full.names = TRUE)
-  ),
-  tar_target(evaluations_pdfs,
-             quarto_to_pdf(path_eval_quarto),
-             pattern = map(path_eval_quarto)),
+#  tar_files(path_eval_quarto,
+#   list.files(here_rel("evaluations"),
+#               pattern = "\\.qmd",
+#              full.names = TRUE)
+#  ),
+#  tar_target(evaluations_pdfs,
+#             quarto_to_pdf(path_eval_quarto),
+#             pattern = map(path_eval_quarto)),
 
   # tar_files(path_exercice_quarto,
   #           list.files(here_rel("exercices"),
