d5702002d15f074c1fb404f9466db7acc43f2e94
Léo Belzile <belzilel@gmail.com>
2023-10-30 20:44:40 -0400

    Built site for gh-pages

--- code/MATH60602-diapos7.R
+++ code/MATH60602-diapos7.R
@@ -1,8 +1,4 @@
-library(ggplot2) # grammaire des graphiques
-library(dplyr) # manipulation de données
-library(patchwork) # combiner les graphiques
-library(MASS) # modèles de régression multinomiale
-library(mice) # données manquantes
+library(mice)
 
 data(manquantes, package = 'hecmulti')
 summary(manquantes)
@@ -13,7 +9,7 @@ summary(manquantes)
 # variables par variables
 apply(manquantes, 2, function(x){mean(is.na(x))})
 # Voir les configurations de valeurs manquantes
-md.pattern(manquantes)
+md.pattern(manquantes) 
 
 # Intensif en calcul, réduire "m" si nécessaire
 impdata <- mice(data = manquantes,
@@ -29,27 +25,16 @@ adj_im <- with(
   data = impdata,
   expr = glm(y ~ x1 + x2 + x3 + x4 + x5 + x6,
              family = binomial))
-# combinaison des résultats
+# combinaison des résultats 
 fit <- pool(adj_im)
 # Tableau résumé avec valeurs-p, degrés de liberté et erreur-types corrigées
 summary(fit)
 
-# Comparer avec le modèle qui utilise uniquement les données complètes
-# Incertitude plus grande parce qu'on a moins de données (notez le message d'avertissement)
-mod_complete <- glm(y ~ .,
-    family = binomial,
-    data = manquantes)
-summary(mod_complete)
-
-# Imputation simple
-mod_simple <- glm(
-  y ~ .,
-  family = binomial,
-  data = complete(impdata, action = 1))
-summary(mod_simple)
-# le modèle fait comme si il n'y avait pas de valeurs manquantes!
-# sous-estimation de l'incertitude, les erreurs-types retournées sont trop petites
-
+## -----------------------------------------------------------------------------
+library(ggplot2)
+library(dplyr)
+library(patchwork)
+data(vote, package = "hecmulti")
 
 ## -----------------------------------------------------------------------------
 data(vote, package = "hecmulti")
@@ -63,6 +48,7 @@ multi1 <- nnet::multinom(
   trace = FALSE)     # infos sur convergence
 
 
+## -----------------------------------------------------------------------------
 # Tableau résumé de l'ajustement
 summary(multi1)
 # Estimations des coefficients
@@ -75,23 +61,106 @@ BIC(multi1)
 # Prédiction: probabilité de chaque modalité
 predict(multi1, type = "probs")
 # Prédiction: classe la plus susceptible
-pred <- predict(multi1, type = "class")
+predict(multi1, type = "class")
 
+
+## -----------------------------------------------------------------------------
 # Modèle avec uniquement l'ordonnée à l'origine (~ 1)
 # Il faut qu'on utilise les mêmes données pour la comparaison!
-multi_cst <- nnet::multinom(catvote ~ 1,
+multi0 <- nnet::multinom(catvote ~ 1,
                          weights = poids,
                          data = vote,
                          subset = age > 30,
                          trace = FALSE)
-head(predict(multi_cst, type = "prob"), n = 3)
 # Test de rapport de vraisemblance
-multi0 <- nnet::multinom(
-  catvote ~ age  + race + revenu + educ + affiliation,
+anova(multi0, multi1)
+
+## -----------------------------------------------------------------------------
+# Modèle logistique à cote proportionnelle
+with(vote, is.ordered(catvote))
+multi2a <- MASS::polr(
+  catvote ~ sexe,
   data = vote,
   subset = age > 30,
   weights = poids,
+  method = "logistic",
+  Hess = TRUE)
+summary(multi2a)
+
+
+-----------------------------------------------------------------------------
+# IC pour beta_x (vraisemblance profilée)
+confint(multi2a)
+
+
+# Critères d'information
+AIC(multi2a); BIC(multi2a)
+# Tableau des coefficients
+# Coefficients (variables explicatives)
+coef(multi2a)
+# Ordonnées à l'origine:
+multi2a$zeta
+
+
+## -----------------------------------------------------------------------------
+multi2b <- nnet::multinom(
+   catvote ~ sexe,
+   data = vote,  
+   subset = age > 30,
+   weights = poids, 
+   trace = FALSE)
+   
+# Combien de paramètres de plus avec modèle logistique?
+# Même nombre d'ordonnées à l'origine, mais pour les p variables explicatives
+# (K-1)*p vs p  paramètres, une différence de (K-2) * p 
+difddl <- (length(multi2a$zeta) - 1) * length(coef(multi2a))
+# Valeur-p du test de rapport de vraisemblance
+# Probabilité qu'une variable khi-deux avec ddl(0)-ddl(1)
+#  dépasse la valeur numérique de la statistique
+pchisq(q = deviance(multi2a) - deviance(multi2b),
+       df = difddl,
+       lower.tail = FALSE)
+
+
+## -----------------------------------------------------------------------------
+multi3a <- MASS::polr(
+  catvote ~ age,
+  data = vote,
+  subset = age > 30,
+  weights = poids,
+  method = "logistic",
+  Hess = TRUE)
+multi3b <- nnet::multinom(catvote ~ age,
+  data = vote,  subset = age > 30,
+  weights = poids, trace = FALSE)
+# Valeur-p du test de rapport de vraisemblance
+pval <- pchisq(q = deviance(multi3a) - deviance(multi3b),
+       df = length(coef(multi2a)),
+       lower.tail = FALSE)
+
+
+## -----------------------------------------------------------------------------
+library(MASS)
+multi3a <- MASS::polr(
+  catvote ~ scale(age, scale = FALSE),
+  data = vote,
+  subset = age > 30,
+  weights = poids,
+  method = "logistic",
+  Hess = TRUE)
+
+multi3b <- nnet::multinom(
+  catvote ~ scale(age, scale = FALSE),
+  data = vote,
+  subset = age > 30,
+  weights = poids,
+  Hess = TRUE,
   trace = FALSE)
-anova(multi0, multi1)
-# On peut le faire directement pour chaque variable à tour de rôle
-car::Anova(multi1, type = 3)
+xpred <- seq(30, 95, by = 0.1) - mean(vote$age)
+nobs <- length(xpred)
+pred1 <- predict(multi3b,
+                 newdata = data.frame(age = xpred),
+                 type = "prob")
+pred2 <- predict(multi3a,
+                 newdata = data.frame(age = xpred),
+                 type = "prob")
